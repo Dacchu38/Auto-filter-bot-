e483d96eec9c3a88317faf5495dfd562cbb0e5f3
Dacchu38 <vd383846@gmail.com>
2024-12-27 22:23:00 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/Kichuprozz/Anya-Forger.git /PiroAutoFilterBot
+  git clone https://github.com/VJBots/Advance-Auto-Filter /Advance-Auto-Filter
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /PiroAutoFilterBot
+  git clone $UPSTREAM_REPO /Advance-Auto-Filter
 fi
-cd /PiroAutoFilterBot
+cd /Advance-Auto-Filter
 pip3 install -U -r requirements.txt
 echo "Starting tgmoviespro1bot...."
 python3 bot.py
